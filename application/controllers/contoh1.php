3903cbb8a8dddc1d7d97a4faf05b37dde193aed0
grselahlystna <graselahellystiana02@gmail.com>
2023-10-10 15:56:26 +0700

    P1 add contoh1.php

--- application/controllers/contoh1.php
+++ application/controllers/contoh1.php
@@ -3,9 +3,9 @@ class Contoh1 extends CI_Controller
 {
     public function index()
     {
+        //contoh1.php//
         echo "<h1>Perkenalan</h1>";
         echo "Nama saya Grasela Hellystiana
         Hobi saya Renang, Membaca, dan Jalan-jalan";
-        //contoh1.php//
     }
 }
