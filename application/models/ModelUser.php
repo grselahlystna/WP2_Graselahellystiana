850dec67942b39ba47fabd45e8afef6cd07a0130
grselahlystna <graselahellystiana02@gmail.com>
2023-10-24 18:31:51 +0700

    P6 Model User

--- application/models/ModelUser.php
+++ application/models/ModelUser.php
@@ -3,8 +3,6 @@ defined('BASEPATH') or exit('No direct script access allowed');
  
 class ModelUser extends CI_Model 
 { 
-    
-    
     public function simpanData($data = null) 
     { 
         $this->db->insert('user', $data); 
